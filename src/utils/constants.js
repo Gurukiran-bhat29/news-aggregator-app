546c4aa52480653c1f39ea6bb7a16fc46a7262d4
gurukiran-bhat-eg <gbhat@eg.dk>
2024-06-17 18:29:45 +0530

    docker file and constant file changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,11 +2,17 @@ export const NEWS_API_KEY = 'apiKey=320b02ba0c1b4d2ca4d45071a34833df';
 
 export const NEWS_API = 'https://newsapi.org/v2/everything?';
 
-export const NEWS_CATEGORY_API = 'https://newsapi.org/v2/top-headlines?';
+export const NEWS_CATEGORY_API =
+  'https://newsapi.org/v2/top-headlines?'
+  ;
 
 export const GAURDIAN_API_KEY = '&api-key=ff0a42c3-9e1f-418a-9a89-3162f75665b0';
 
-export const GAURDIAN_API = 'https://content.guardianapis.com/';
+export const GAURDIAN_API =
+  'https://content.guardianapis.com/search?page=2&api-key=ff0a42c3-9e1f-418a-9a89-3162f75665b0'
+  ;
+
+export const GAURDIAN_CATEGORY_API = 'https://content.guardianapis.com/';
 
 export const categoryLists = [
   { id: 'technology', name: 'Technology' },
